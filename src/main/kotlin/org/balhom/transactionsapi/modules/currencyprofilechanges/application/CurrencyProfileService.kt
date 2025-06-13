40b5830506969b4d2a04b3a6165ad9900f769008
fabbo-repo <fscherle98@gmail.com>
2025-06-13 12:33:53 +0200

    Transactional fix

--- src/main/kotlin/org/balhom/transactionsapi/modules/currencyprofilechanges/application/CurrencyProfileService.kt
+++ src/main/kotlin/org/balhom/transactionsapi/modules/currencyprofilechanges/application/CurrencyProfileService.kt
@@ -1,14 +1,12 @@
 package org.balhom.transactionsapi.modules.currencyprofilechanges.application
 
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.transaction.Transactional
 import org.balhom.transactionsapi.modules.currencyprofilechanges.domain.clients.CurrencyProfileReferenceClient
 import org.balhom.transactionsapi.modules.currencyprofilechanges.domain.exceptions.CurrencyProfileReferenceNotFoundException
 import org.balhom.transactionsapi.modules.currencyprofilechanges.domain.models.CurrencyProfileReference
 import java.util.*
 
 @ApplicationScoped
-@Transactional
 class CurrencyProfileService(
     private val currencyProfileReferenceClient: CurrencyProfileReferenceClient,
 ) {
